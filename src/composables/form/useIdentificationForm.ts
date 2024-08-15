e500aef84ef8adc3055b9df88c1bd78ecd408474
Bernardo <berbatovrobben@gmail.com>
2024-08-15 01:59:31 -0300

    fix: composables global

--- src/composables/form/useIdentificationForm.ts
+++ src/composables/form/useIdentificationForm.ts
@@ -1,25 +1,25 @@
 import { ref } from 'vue'
 import { IdentificationForm } from '@/types/form/IdentificationFormType'
 
-const form = ref<IdentificationForm>({
-  name: '',
-  phone: '',
-  email: '',
-  age: 0,
-  avatar: null,
-})
-
-const resetForm = () => {
-  form.value = {
+export const useIdentificationForm = () => {
+  const form = ref<IdentificationForm>({
     name: '',
     phone: '',
     email: '',
     age: 0,
     avatar: null,
-  }
-}
+  })
 
-export const useIdentificationForm = () => {
+  const resetForm = () => {
+    form.value = {
+      name: '',
+      phone: '',
+      email: '',
+      age: 0,
+      avatar: null,
+    }
+  }
+  
   return {
     form,
     resetForm,
